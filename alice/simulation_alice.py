3b614bc1a407e62a27f28c916b32c524cf03f565
alice <aligao@ucdavis.edu>
2019-01-31 14:19:03 -0800

    measure avg time to transmit a packet

--- alice/simulation_alice.py
+++ alice/simulation_alice.py
@@ -2,14 +2,12 @@ import simpy
 import random
 import math
 
-RANDOM_SEED = 29
-SIM_TIME = 100000 #1000 minutes = 1000000 msec/ticks
-MU = 1
+#RANDOM_SEED = 29
+SIM_TIME = 36000000 #1000 minutes = 1000000 sec
 FRAME = 1000
 
 packet_number = 0
-queue_len = 0
-rate = 1/15000
+rate = 1/900000 #15 minutes
 
 ACK_TIME = 5
 TRANSMIT_SLOT = 9
@@ -21,29 +19,7 @@ class Packet:
     def __init__(self, identifier, arrival_time):
         self.identifier = identifier
         self.arrival_time = arrival_time
-
-
-def create_packet(env, arrival_rate, n0):
-    global packet_number
-    global queue_len
-    while True:
-        # Infinite loop for generating packets
-        # change to every time increment, then determine for every host is there a packet arrival
-        yield env.timeout(random.expovariate(arrival_rate))
-        print("pkr generation: {0}".format(env.now))
-
-        packet_number += 1
-        arrival_time = env.now
-        # print(self.num_pkt_total, "packet arrival")
-        new_packet = Packet(packet_number, arrival_time)
-        #if self.flag_processing == 0:
-        #    self.flag_processing = 1
-        #    idle_period = env.now - self.start_idle_time
-        #    self.Server_Idle_Periods.addNumber(idle_period)
-        # print("Idle period of length {0} ended".format(idle_period))
-        queue_len += 1
-        #env.process(process_packet(env, new_packet))
-        n0.increment_queue(new_packet)
+        self.transmit_time = 0
 
 
 class Node:
@@ -61,21 +37,27 @@ class Node:
         self.idle_time = 0
         self.start_time = 0
 
+        self.transmit_attempt = 0
+        self.pkt = None
+
     def transmit(self, env):
         while True:
             idle_start = env.now
             while (env.now - idle_start) < FRAME:
                 sleep_time = random.expovariate(self.transmit_rate)
-                self.idle_time += sleep_time
                 yield env.timeout(sleep_time)
+                self.idle_time += sleep_time
                 self.start_time = env.now
+
                 if self.queue > 0:
+                    self.pkt.transmit_time += sleep_time
+                    self.transmit_attempt += 1
                     try:
-                        print("start transmitting at: {0}".format(env.now))
+                        #print("start transmitting at: {0}".format(env.now))
                         self.transmitting = 1
                         yield env.timeout(TRANSMIT_SLOT)
                         self.transmitting = 0
-                        print("stop transmitting at {0}".format(env.now))
+                        #print("stop transmitting at {0}".format(env.now))
                     except simpy.Interrupt as i:
                         if i.cause == "ack":
                             print("ack received at {0}, queue: {1}".format(env.now, self.queue))
@@ -83,7 +65,9 @@ class Node:
                             self.receive_ack(env)
                             self.decrement_queue()
                             yield env.timeout(TRANSMIT_SLOT)
-                            idle_start = FRAME
+                            #print("stop transmitting at {0}".format(env.now))
+                            #break
+                    self.pkt.transmit_time += TRANSMIT_SLOT
 
     def transmit_ack(self):
         yield env.timeout(ACK_TIME)
@@ -95,7 +79,7 @@ class Node:
             yield env.timeout(sleep_time)
             #yield env.timeout(random.expovariate(self.wake_up_rate))
             #print("start receiving at: {0}".format(env.now))
-            start_time = env.now
+            self.start_time = env.now
             try:
                 self.receiving = 1
                 yield env.timeout(TRANSMIT_SLOT)
@@ -111,6 +95,7 @@ class Node:
 
     def increment_queue(self, pkt):
         self.queue += 1
+        self.pkt = pkt
 
     def decrement_queue(self):
         self.queue -= 1
@@ -120,6 +105,7 @@ class WatchNode:
         self.num_successful_packet = 0
         self.n0 = n0
         self.n1 = n1
+        self.packet_list = []
 
     def checkSuccess(self, env, tx, rx):
         while True:
@@ -132,22 +118,46 @@ class WatchNode:
                 rx.interrupt(self.n0.start_time)
             yield env.timeout(1)
 
+    def create_packet(self, env, arrival_rate):
+        global packet_number
+        while True:
+            # Infinite loop for generating packets
+            #yield env.timeout(random.expovariate(arrival_rate))
+            #print("pkr generation: {0}".format(env.now))
+            yield env.timeout(900000)
+
+            packet_number += 1
+            arrival_time = env.now
+            new_packet = Packet(packet_number, arrival_time)
+            self.packet_list.append(new_packet)
+            self.n0.increment_queue(new_packet)
+
+
 if __name__ == '__main__':
     env = simpy.Environment()
-    t_rate = 1/1000
-    r_rate = 1/50
+    t_rate = 1/4000
+    r_rate = 1/100
     n0 = Node(t_rate, r_rate, 0, env)
     n1 = Node(t_rate, r_rate, 1, env)
     watcher = WatchNode(env, n0, n1)
 
-    env.process(create_packet(env, rate, n0))
+    env.process(watcher.create_packet(env, rate))
     tx = env.process(n0.transmit(env))
     rx = env.process(n1.receive(env))
     env.process(watcher.checkSuccess(env, tx, rx))
     env.run(until=SIM_TIME)
 
-    print("---results---")
+    print("\n---results---")
     print("number of packets generated: {0}".format(packet_number))
-    print("successful transmition rate: {0}".format(watcher.num_successful_packet / packet_number))
-    print("node 0 sleep time: {0}".format(n0.idle_time))
-    print("node 1 sleep time: {0}".format(n1.idle_time))
+    print("number of packets transmitted: {0}".format(watcher.num_successful_packet))
+    print("successful transmition rate: {0:.3f}%".format(watcher.num_successful_packet / packet_number))
+    print("number of attempted transmission rounds: {0}".format(n0.transmit_attempt))
+    print("node 0 sleep time: {0:.5f}%".format(n0.idle_time / SIM_TIME))
+    print("node 1 sleep time: {0:.5f}%\n".format(n1.idle_time / SIM_TIME))
+
+    total_transmit_time = 0
+    for p in watcher.packet_list:
+        print("{0}: {1:.3f}".format(p.identifier, p.transmit_time))
+        total_transmit_time += p.transmit_time
+    avg_transmit_time = total_transmit_time / packet_number
+    print("avg time to transmit: {0:.3f}".format(avg_transmit_time))
