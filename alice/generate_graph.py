68598317f9e56bd7f77a1744bec6906d835d48e4
aligao@ucdavis.edu <aligao@ucdavis.edu>
2019-02-13 18:29:20 -0800

    3D graph

--- alice/generate_graph.py
+++ alice/generate_graph.py
@@ -1,15 +1,18 @@
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import pandas as pd
+import numpy as np
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
 import mplcursors
 
 def read_data(file):
     ret = pd.read_csv(file)
-    print(ret)
+    #print(ret)
     return ret
 
 def same_tx_rx_rate(varying_rates):
-    plt.figure()
+    fig1 = plt.figure(1)
     plt.subplot(1, 2, 1)
     # fig, ax = plt.subplots()
 
@@ -27,10 +30,53 @@ def same_tx_rx_rate(varying_rates):
     plt.xlabel('Avg Time in between Transmission')
     plt.ylabel('seconds')
     plt.title('Avg Transmission Time per Pkt')
-    plt.show()
+    fig1.show()
 
 def different_tx_rx_rate(differing_rates):
-    fig = plt.figure()
+    tx_rate = differing_rates['tx_rate'].unique()
+    rx_rate = differing_rates['rx_rate'].unique()
+    X, Y = np.meshgrid(tx_rate, rx_rate)
+
+    tx_time = differing_rates['tx_time']
+    Z = tx_time.values.reshape(len(X), len(Y))
+    print(Z)
+
+    fig = plt.figure(2)
+    ax = fig.gca(projection='3d')
+
+    # Plot the surface.
+    surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
+                           linewidth=0, antialiased=False)
+
+    # Customize the z axis.
+    ax.set_zlim(-1, 2)
+    ax.zaxis.set_major_locator(LinearLocator(10))
+    ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+    plt.xlabel('Avg time in between tx (ms)')
+    plt.ylabel('Avg time in between rx (ms)')
+    plt.title('Tx_time (percentage)')
+
+    # Add a color bar which maps values to colors.
+    fig.colorbar(surf, shrink=0.5, aspect=5)
+
+    fig.show()
+
+def rx_rate3D(differing_rates):
+    tx_rate = differing_rates['tx_rate'].unique()
+    rx_rate = differing_rates['rx_rate'].unique()
+    #print(differing_rates['tx_rate'])
+    #print(differing_rates['rx_rate'])
+
+    X, Y = np.meshgrid(tx_rate, rx_rate)
+
+    # R = X + Y
+    # print(R)
+
+    rx_time = differing_rates['rx_time']
+    Z = rx_time.values.reshape(len(X), len(Y))
+    print(Z)
+
+    fig = plt.figure(3)
     ax = fig.gca(projection='3d')
 
 
@@ -39,18 +85,54 @@ def different_tx_rx_rate(differing_rates):
                            linewidth=0, antialiased=False)
 
     # Customize the z axis.
-    ax.set_zlim(-1.01, 1.01)
+    ax.set_zlim(-1, 10)
     ax.zaxis.set_major_locator(LinearLocator(10))
     ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+    plt.xlabel('Avg time in between tx (ms)')
+    plt.ylabel('Avg time in between rx (ms)')
+    plt.title('Rx_time (percentage)')
+
+    # Add a color bar which maps values to colors.
+    fig.colorbar(surf, shrink=0.5, aspect=5)
+
+    fig.show()
+
+def avg_time_pkt3D(differing_rates):
+    tx_rate = differing_rates['tx_rate'].unique()
+    rx_rate = differing_rates['rx_rate'].unique()
+
+    X, Y = np.meshgrid(tx_rate, rx_rate)
+
+    avg_pkt_time = differing_rates['avg_transmission_time_per_pkt']
+    Z = avg_pkt_time.values.reshape(len(X), len(Y))
+    print(Z)
+
+    fig = plt.figure(4)
+    ax = fig.gca(projection='3d')
+
+    # Plot the surface.
+    surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
+                           linewidth=0, antialiased=False)
+
+    # Customize the z axis.
+    ax.set_zlim(0, 11000000)
+    ax.zaxis.set_major_locator(LinearLocator(10))
+    ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+    plt.xlabel('Avg time in between tx (ms)')
+    plt.ylabel('Avg time in between rx (ms)')
+    plt.title('Avg time to transmit a packet (ms)')
 
     # Add a color bar which maps values to colors.
     fig.colorbar(surf, shrink=0.5, aspect=5)
 
     plt.show()
 
-
 if __name__ == '__main__':
     varying_rates = read_data("varying_rate.csv")
     same_tx_rx_rate(varying_rates)
 
-    #differing_rates = read_data("differing_rates.csv")
+    differing_rates = read_data("../Dmitry/02-10-results.txt")
+    different_tx_rx_rate(differing_rates)
+    rx_rate3D(differing_rates)
+    avg_time_pkt3D(differing_rates)
+
