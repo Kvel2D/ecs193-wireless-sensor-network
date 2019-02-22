2626ac26a36cae6ac855d13adc95fb669d13f54b
Adrian Wang <atwang@ucdavis.edu>
2019-02-21 16:56:44 -0800

    Add Tree functionality
    Signed-off-by: Adrian Wang <atwang@ucdavis.edu>

--- adrian/sim_env.py
+++ adrian/sim_env.py
@@ -4,6 +4,8 @@ import string
 import networkx as nx
 import simpy
 
+finish_list = []
+
 
 def generate_message(env, start_node, ticks_between_packets):
     '''
@@ -21,46 +23,69 @@ def generate_message(env, start_node, ticks_between_packets):
     while True:
         # start_node.message_queue.append((i, message_list[i]))
         for recv_id in start_node.receiver_lists.keys():
-            start_node.receiver_lists[recv_id].append([i] + [env.now])
+            start_node.receiver_lists[recv_id].append([(i, env.now)])
         yield env.timeout(ticks_between_packets)
         i += 1
 
 
-finish_list = []
+def generate_multiple_messages(env, generator_nodes, ticks_between_packets):
+    '''
+    Generates packets simultaneously in start_nodes every ticks_between_packets
+    Args:
+        env ():
+        generator_nodes ():
+        ticks_between_packets ():
+
+    Returns: None
+    '''
+    i = 0
+    while True:
+        for node in generator_nodes:
+            for recv_id in node.receiver_lists.keys():
+                node.receiver_lists[recv_id].append([(i, env.now)] + [(node.id, env.now)])
+            i += 1
+        yield env.timeout(ticks_between_packets)
 
 
-def send_protocol(sender, receiver, message):
+def send_protocol(sender, receiver_id, message):
     '''
 
     Args:
-        env: SimPy.Environment instance
-        graph: NetworkX DiGraph instance
         sender: Node instance of sending node
-        receiver: Node id of the receiving node
+        receiver_id: Node id of the receiving node
         message: the message
 
     Returns: True if the message successfully transmitted, False otherwise
 
     '''
-    if sender.is_gateway:  # temporary implementation of collecting messages from last node in chain
-        finish_list.append(message + [sender.env.now])
+    global finish_list
+    if sender.is_gateway:
+        finish_list.append(message + [(sender.id, sender.env.now)])
         sender.message_transmitted_alert.succeed()
         return sender.message_transmitted_alert
-    node = sender.graph.nodes[receiver]['node']
-    if receiver in sender.graph[sender.id]:  # if this edge exists, then receiver is in RCV_MODE
-        for recv_id in node.receiver_lists.keys():
-            node.receiver_lists[recv_id].append(message + [sender.env.now])
+    node = sender.graph.nodes[receiver_id]['node']
+    if receiver_id in sender.graph[sender.id]:  # if this edge exists, then receiver is in RCV_MODE
+        if node.is_gateway:  # if the recipient node is gateway, just append to finish_list
+            finish_list.append(message + [(receiver_id, sender.env.now)])
+        else:
+            for recv_id in node.receiver_lists.keys():
+                node.receiver_lists[recv_id].append(message + [(receiver_id, sender.env.now)])
         node.message_received_alert.succeed()
         sender.message_transmitted_alert.succeed()  # placeholder for ACK
     return sender.message_transmitted_alert
 
 
 def receive_protocol(receiver):
+    global finish_list
     for sender_id, _ in receiver.graph.in_edges(receiver.id):
         node = receiver.graph.nodes[sender_id]['node']
         if node.sending_to == receiver.id:  # TODO: implement listening to different channels
-            for recv_id in receiver.receiver_lists.keys():
-                receiver.receiver_lists[recv_id].append(node.transmitting_message + [receiver.env.now])
+            if receiver.is_gateway:
+                finish_list.append(node.transmitting_message + [(receiver.id, receiver.env.now)])
+            else:
+                for recv_id in receiver.receiver_lists.keys():
+                    receiver.receiver_lists[recv_id].append(
+                            node.transmitting_message + [(receiver.id, receiver.env.now)])
             node.message_transmitted_alert.succeed()
             receiver.message_received_alert.succeed()
             break
@@ -75,7 +100,7 @@ class Node:
 
     def __init__(self, env, graph, _id, mq_length, senders, receivers, rx_sleep_ticks = 900, tx_sleep_ticks = 900,
                  receive_ticks = 9,
-                 transmit_ticks = 9, is_gateway = False):
+                 transmit_ticks = 9, is_gateway = False, is_generator = False):
         self.env = env
         self.graph = graph
         self.id = _id
@@ -85,6 +110,7 @@ class Node:
         self.receive_ticks = receive_ticks
         self.transmit_ticks = transmit_ticks
         self.is_gateway = is_gateway
+        self.is_generator = is_generator
         self.action = env.process(self.run())
         self.message_transmitted_alert = None
         self.message_received_alert = None
@@ -103,36 +129,7 @@ class Node:
 
     def run(self):
         while True:
-            if any(self.receiver_lists.values()):  # if the node has any messages to transmit
-                # slp mode
-                self.mode = Node.Mode.slp
-                last = self.env.now
-                yield self.env.timeout(random.expovariate(1 / self.tx_sleep_ticks))
-                self.increment_timer(self.env.now - last)
-                # tx mode
-                self.mode = Node.Mode.tx
-                last = self.env.now
-                send_time = 0
-                active_lists = {receiver: queue for receiver, queue in self.receiver_lists.items() if queue}
-                for receiver, queue in active_lists.items():
-                    self.sending_to = receiver
-                    message = queue[0]
-                    self.transmitting_message = message
-                    self.message_transmitted_alert = self.env.event()
-                    if not send_protocol(self, receiver, self.transmitting_message).processed:
-                        yield self.message_transmitted_alert | self.env.timeout(self.receive_ticks - send_time)
-                    send_time = self.env.now - last
-                    try:
-                        if self.message_transmitted_alert.ok:
-                            self.receiver_lists[receiver].pop(0)
-                    except AttributeError:
-                        pass
-                    if not send_time < self.transmit_ticks:
-                        break
-                self.sending_to = None
-                self.message_transmitted_alert = None
-                self.increment_timer(self.env.now - last)
-            else:  # enter receiving mode
+            if self.is_gateway:  # gateway will only receive
                 # slp mode
                 self.mode = Node.Mode.slp
                 last = self.env.now
@@ -153,6 +150,62 @@ class Node:
                 for i in self.senders:
                     self.graph.remove_edge(i, self.id)  # end receive mode
                 self.increment_timer(self.env.now - last)
+                # print('TESTING')
+                # for k, v in self.receiver_lists.items():
+                #     print('{}, {}'.format(k, v))
+                # global finish_list
+                # finish_list.append(self.receiver_lists[self.id].pop(0))
+            else:
+                if any(self.receiver_lists.values()):  # if the node has any messages to transmit
+                    # slp mode
+                    self.mode = Node.Mode.slp
+                    last = self.env.now
+                    yield self.env.timeout(random.expovariate(1 / self.tx_sleep_ticks))
+                    self.increment_timer(self.env.now - last)
+                    # tx mode
+                    self.mode = Node.Mode.tx
+                    last = self.env.now
+                    send_time = 0
+                    active_lists = {receiver: queue for receiver, queue in self.receiver_lists.items() if queue}
+                    for receiver, queue in active_lists.items():
+                        self.sending_to = receiver
+                        message = queue[0]
+                        self.transmitting_message = message
+                        self.message_transmitted_alert = self.env.event()
+                        if not send_protocol(self, receiver, self.transmitting_message).processed:
+                            yield self.message_transmitted_alert | self.env.timeout(self.receive_ticks - send_time)
+                        send_time = self.env.now - last
+                        try:
+                            if self.message_transmitted_alert.ok:
+                                self.receiver_lists[receiver].pop(0)
+                        except AttributeError:
+                            pass
+                        if not send_time < self.transmit_ticks:
+                            break
+                    self.sending_to = None
+                    self.message_transmitted_alert = None
+                    self.increment_timer(self.env.now - last)
+                else:  # enter receiving mode
+                    # slp mode
+                    self.mode = Node.Mode.slp
+                    last = self.env.now
+                    yield self.env.timeout(random.expovariate(1 / self.tx_sleep_ticks))
+                    self.increment_timer(self.env.now - last)
+                    # rx mode
+                    self.mode = Node.Mode.rx
+                    for i in self.senders:
+                        self.graph.add_edge(i, self.id)  # add these edges to allow transmission
+                    last = self.env.now
+                    self.message_received_alert = self.env.event()
+                    try:
+                        if receive_protocol(self).ok:
+                            pass
+                    except AttributeError:
+                        yield self.message_received_alert | self.env.timeout(self.receive_ticks)
+                    self.message_received_alert = None
+                    for i in self.senders:
+                        self.graph.remove_edge(i, self.id)  # end receive mode
+                    self.increment_timer(self.env.now - last)
 
     def increment_timer(self, time):
         if self.mode == Node.Mode.tx:
@@ -178,29 +231,29 @@ def tester(num_nodes = 4, mq_length = 10, rx_sleep_ticks = 1000, tx_sleep_ticks 
     Returns:
 
     '''
+    graph = nx.DiGraph()
+    env = simpy.Environment()
+    for i in range(num_nodes):
+        if i == 0:
+            node = Node(env, graph, i, mq_length, [], [i + 1], rx_sleep_ticks = rx_sleep_ticks,
+                        tx_sleep_ticks = tx_sleep_ticks)
+            env.process(generate_message(env, node, ticks_between_packets))
+        elif i == num_nodes - 1:
+            node = Node(env, graph, i, mq_length, [i - 1], [i + 1], rx_sleep_ticks = rx_sleep_ticks,
+                        tx_sleep_ticks = tx_sleep_ticks, is_gateway = True)
+        else:
+            node = Node(env, graph, i, mq_length, [i - 1], [i + 1], rx_sleep_ticks = rx_sleep_ticks,
+                        tx_sleep_ticks = tx_sleep_ticks)
+        graph.add_node(node.id, node = node)
+    env.run(until = run_until)
     with open('results.csv', 'a') as file:
-        graph = nx.DiGraph()
-        env = simpy.Environment()
-        for i in range(num_nodes):
-            if i == 0:
-                node = Node(env, graph, i, mq_length, [], [i + 1], rx_sleep_ticks = rx_sleep_ticks,
-                            tx_sleep_ticks = tx_sleep_ticks)
-                env.process(generate_message(env, node, ticks_between_packets))
-            elif i == num_nodes - 1:
-                node = Node(env, graph, i, mq_length, [i - 1], [i + 1], rx_sleep_ticks = rx_sleep_ticks,
-                            tx_sleep_ticks = tx_sleep_ticks, is_gateway = True)
-            else:
-                node = Node(env, graph, i, mq_length, [i - 1], [i + 1], rx_sleep_ticks = rx_sleep_ticks,
-                            tx_sleep_ticks = tx_sleep_ticks)
-            graph.add_node(node.id, node = node)
-        env.run(until = run_until)
         file.write('{},{},'.format(tx_sleep_ticks, rx_sleep_ticks))
         tx_time_percent = graph.nodes[1]['node'].transmit_for / run_until * 100
         rx_time_percent = graph.nodes[1]['node'].receive_for / run_until * 100
         file.write('{},{},'.format(tx_time_percent, rx_time_percent))
         total_packet_create_to_gateway_time = 0
         for l in finish_list:
-            total_packet_create_to_gateway_time += l[-1] - l[1]
+            total_packet_create_to_gateway_time += l[-1][1] - l[1][1]
         avg_packet_create_to_gateway_time = total_packet_create_to_gateway_time / len(finish_list)
         avg_packet_node_to_node_time = avg_packet_create_to_gateway_time / (num_nodes - 1)
         file.write('{}\n'.format(avg_packet_node_to_node_time))
@@ -215,10 +268,48 @@ def tester(num_nodes = 4, mq_length = 10, rx_sleep_ticks = 1000, tx_sleep_ticks 
     print('\nSuccessfully passed messages:\n\t\t{}'.format(finish_list))
 
 
+def tree_tester(branch_factor = 2, height = 3, mq_length = 10, rx_sleep_ticks = 1000, tx_sleep_ticks = 1000,
+                ticks_between_packets = 15 * 60 * 1000, run_until = 12 * 60 * 60 * 1000):
+    env = simpy.Environment()
+    graph = nx.balanced_tree(branch_factor, height, nx.DiGraph)
+    graph = graph.reverse()  # we want the edges to all point to root
+    generator_nodes = []
+    for i in graph.nodes:
+        senders = [j for j, _ in graph.in_edges(i)]
+        receivers = [j for _, j in graph.out_edges(i)]
+        node = Node(env, graph, i, mq_length, senders, receivers, rx_sleep_ticks, tx_sleep_ticks)
+        if not receivers:  # this is a gateway
+            node.is_gateway = True
+        elif not senders:  # this is a start_node
+            node.is_generator = True
+            generator_nodes.append(node)
+        graph.nodes[i]['node'] = node
+    for i in graph.nodes:  # remove the edges for simulation
+        graph.remove_edges_from(list(graph.out_edges(i)))  # need to create separate copy of the out_edges
+    # for i in graph.nodes:
+    #     print('{}: {}, {}'.format(graph.nodes[i]['node'].id, graph.nodes[i]['node'].receivers,
+    #                               graph.nodes[i]['node'].senders))
+    # print()
+    env.process(generate_multiple_messages(env, generator_nodes, ticks_between_packets))
+    env.run(until = run_until)
+    for node in graph.nodes:
+        _node = graph.nodes[node]['node']
+        print('{}: transmitted for: {}, received for: {}, slept for: {}'.format(node, _node.transmit_for,
+                                                                                _node.receive_for, _node.sleep_for))
+        print('\tUnable to transmit messages:')
+        for recv_id in _node.receiver_lists.keys():
+            print('\t\tTo {}: {}'.format(recv_id, _node.receiver_lists[recv_id]))
+    print('\nSuccessfully passed messages:')
+    for l in finish_list:
+        print('\t\tMessage {} generated at: {}'.format(l[0][0], l[0][1]))
+        print('\t\t\t(Node #, Time): {}'.format(l[1:]))
+
+
 if __name__ == '__main__':
     sleep_rates = [100, 200, 400, 800, 1000, 1500, 2000, 2500, 3000, 4000, 5000, 6000, 7000, 8000]
-    for rx_rate_i in range(len(sleep_rates)):
-        for tx_rate_i in range(len(sleep_rates)):
-            TX_SLEEP_RATE = sleep_rates[tx_rate_i]
-            RX_SLEEP_RATE = sleep_rates[rx_rate_i]
-            tester(rx_sleep_ticks = RX_SLEEP_RATE, tx_sleep_ticks = TX_SLEEP_RATE)
+    tree_tester(branch_factor = 3, run_until = 1000 * 60 * 60)
+    # for rx_rate_i in range(len(sleep_rates)):
+    #     for tx_rate_i in range(len(sleep_rates)):
+    #         TX_SLEEP_RATE = sleep_rates[tx_rate_i]
+    #         RX_SLEEP_RATE = sleep_rates[rx_rate_i]
+    #         tester(rx_sleep_ticks = RX_SLEEP_RATE, tx_sleep_ticks = TX_SLEEP_RATE)
