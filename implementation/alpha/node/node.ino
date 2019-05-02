de7c90da0e6d1d23e86edd2171d3002a1b3683f3
Kvel2D <kvelyognaut@gmail.com>
2019-05-01 19:41:16 -0700

    make packet_queue_age_ms an array

--- implementation/alpha/node/node.ino
+++ implementation/alpha/node/node.ino
@@ -48,7 +48,7 @@ float current_frequency = 0.0f;
 uint32_t last_reading_time = 0;
 uint32_t last_healthPacket_time = 0;
 Queue packet_queue;
-uint16_t packet_queue_age_ms;
+uint16_t packet_queue_age_ms[NUM_SENSORS];
 static int8_t packet_number = 0;
 bool do_first_health_packet = true;
 bool do_first_reading_packet = true;
