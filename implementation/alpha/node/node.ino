d0a168dc79aac9fdf9aacc5e5505ec1f756eb258
Adrian Wang <atwang@ucdavis.edu>
2019-05-08 18:04:55 -0700

    fix format

--- implementation/alpha/node/node.ino
+++ implementation/alpha/node/node.ino
@@ -27,19 +27,19 @@ uint8_t my_id = EEPROM.read(EEPROM.length() - 1);
 NodeData my_data;
 NodeData parent_data;
 
-#define PRINT_DEBUG false
+#define PRINT_DEBUG     false
 #define WAIT_FOR_SERIAL false
-#define RF69_FREQ 433.0
-#define RFM69_CS 8
-#define RFM69_INT 7
-#define RFM69_RST 4
-#define LED_PIN 13
-#define LED_PERIOD (60ul * 1000ul)
+#define RF69_FREQ       433.0
+#define RFM69_CS        8
+#define RFM69_INT       7
+#define RFM69_RST       4
+#define LED_PIN         13
+#define LED_PERIOD      (60ul * 1000ul)
 
-#define PACKET_PERIOD (1000ul * 60ul * 5ul)
-#define HEALTH_PACKET_PERIOD (1000ul * 60ul * 60ul)
-#define RX_RATE (600.0f)
-#define TX_RATE (200.0f)
+#define PACKET_PERIOD   (1000ul * 60ul * 5ul)
+#define HEALTH_PACKET_PERIOD  (1000ul * 60ul * 60ul)
+#define RX_RATE      (600.0f)
+#define TX_RATE      (200.0f)
 
 RH_RF69 rf69(RFM69_CS, RFM69_INT);
 RHReliableDatagram rf69_manager(rf69, my_id);
@@ -59,67 +59,66 @@ static uint32_t last_receive = 0;
 static uint32_t current_tx_sleep = 0;
 static uint32_t current_rx_sleep = 0;
 
-void setup() {
-  pinMode(LED_PIN, OUTPUT);
+void setup() {  
+    pinMode(LED_PIN, OUTPUT);
 
-  Serial.begin(115200);
+    Serial.begin(115200);
 
-  if (WAIT_FOR_SERIAL) {
-    while (!Serial) {
-      delay(1);
+    if (WAIT_FOR_SERIAL) {
+        while (!Serial) {
+            delay(1);
+        }
     }
-  }
 
-  // Read node and parent data
-  memcpy_P(&my_data, &tree_data[my_id], sizeof(NodeData));
-  if (my_data.parent != NO_ID) {
-    memcpy_P(&parent_data, &tree_data[my_data.parent], sizeof(NodeData));
-  }
-
-  // NOTE: has to happen here right after serial connection is made
-  if (my_data.has_sensor) {
-    setup_sensors();
-  }
-
-  pinMode(2, OUTPUT);  // set up for the sensor voltage
-  pinMode(RFM69_RST, OUTPUT);
-  digitalWrite(RFM69_RST, LOW);
-
-  // reset radio
-  // NOTE: is this needed?
-  digitalWrite(RFM69_RST, HIGH);
-  delay(10);
-  digitalWrite(RFM69_RST, LOW);
-  delay(10);
-
-  if (!rf69_manager.init()) {
-    while (true) {
-      delay(1);
-      Serial.println("RFM69 radio init failed");
+    // Read node and parent data
+    memcpy_P(&my_data, &tree_data[my_id], sizeof(NodeData));
+    if (my_data.parent != NO_ID) {
+        memcpy_P(&parent_data, &tree_data[my_data.parent], sizeof(NodeData));
     }
-  }
 
-  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM (for
-  // low power module) No encryption
-  if (!rf69.setFrequency(RF69_FREQ)) {
-    while (true) {
-      delay(1);
-      Serial.println("setFrequency failed");
+    // NOTE: has to happen here right after serial connection is made
+    if (my_data.has_sensor) {
+        setup_sensors();
     }
-  }
 
-  // If you are using a high power RF69 eg RFM69HW, you *must* set a Tx power
-  // with the ishighpowermodule flag set like this: power range is 14-20, 2nd
-  // arg must be true for 69HCW
-  rf69.setTxPower(20, true);
+    pinMode(2, OUTPUT); // set up for the sensor voltage
+    pinMode(RFM69_RST, OUTPUT);
+    digitalWrite(RFM69_RST, LOW);
 
-  // The encryption key has to be the same as the one in the server
-  uint8_t key[] = {0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
-                   0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08};
-  rf69.setEncryptionKey(key);
+    // reset radio
+    // NOTE: is this needed?
+    digitalWrite(RFM69_RST, HIGH);
+    delay(10);
+    digitalWrite(RFM69_RST, LOW);
+    delay(10);
 
-  rf69_manager.setRetries(0);
-  rf69_manager.setTimeout(10);
+    if (!rf69_manager.init()) {
+        while (true) {
+            delay(1);
+            Serial.println("RFM69 radio init failed");
+        }
+    }
+
+    // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM (for low power module)
+    // No encryption
+    if (!rf69.setFrequency(RF69_FREQ)) {
+        while (true) {
+            delay(1);
+            Serial.println("setFrequency failed");
+        }
+    }
+
+    // If you are using a high power RF69 eg RFM69HW, you *must* set a Tx power with the
+    // ishighpowermodule flag set like this:
+    // power range is 14-20, 2nd arg must be true for 69HCW
+    rf69.setTxPower(20, true);
+
+    // The encryption key has to be the same as the one in the server
+    uint8_t key[] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08};
+    rf69.setEncryptionKey(key);
+
+    rf69_manager.setRetries(0);
+    rf69_manager.setTimeout(10);
 
   // generate on startup
   if (do_first_health_packet) health_packet_generate();
@@ -128,64 +127,65 @@ void setup() {
 }
 
 int free_ram() {
-  extern int __heap_start, *__brkval;
-  int v;
-  return (int)&v - (__brkval == 0 ? (int)&__heap_start : (int)__brkval);
+    extern int __heap_start, *__brkval; 
+    int v; 
+    return (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval); 
 }
 
 float expovariate(float rate) {
-  float k = -((float)random(0, RAND_MAX) / (RAND_MAX + 1));
-  return -log(1.0f - k) / (1 / rate);
+    float k = -((float) random(0, RAND_MAX) / (RAND_MAX + 1));
+    return -log(1.0f - k) / (1 / rate);
 }
 
 uint32_t convert_to_sleepydog_time(uint32_t time) {
-  uint32_t remainder = time;
-  remainder = remainder % 250;
-  remainder = remainder % 15;
-  uint32_t sleepydog_time = time - remainder;
-  if (sleepydog_time < 15) {
-    sleepydog_time = 15;
-  }
-  return sleepydog_time;
+    uint32_t remainder = time;
+    remainder = remainder % 250;
+    remainder = remainder % 15;
+    uint32_t sleepydog_time = time - remainder;
+    if (sleepydog_time < 15) {
+        sleepydog_time = 15;
+    }
+    return sleepydog_time;
 }
 
 void blink_led_periodically() {
-  static uint32_t prev_time = 0;
+    static uint32_t prev_time = 0;
 
-  uint32_t current_time = millis();
-
-  if (current_time - prev_time > LED_PERIOD) {
-    prev_time = current_time;
-    digitalWrite(LED_PIN, HIGH);
-    delay(1000);
-    digitalWrite(LED_PIN, LOW);
-  }
+    uint32_t current_time = millis();
+    
+    if (current_time - prev_time > LED_PERIOD) {
+        prev_time = current_time;
+        digitalWrite(LED_PIN, HIGH);
+        delay(1000);
+        digitalWrite(LED_PIN, LOW);
+    }
 }
 
 void print_packet(struct Packet p) {
-  // 1 uint32 (10 chars)
-  // 3 uint8 (3 chars)
-  // 6 floats (formatted as -123.45, so 7 chars)
-  // 9 commas
-  // null-terminator
-  // 1 more char for leeway
-  static char print_packet_buffer[10 + 3 * 3 + 6 * 6 + 9 + 1 + 1];
-  int print_size =
-      snprintf(print_packet_buffer, sizeof(print_packet_buffer),
-               "%lu,%u,%u,%u,%d.%01d,%d.%01d,%d.%01d,%d.%01d,%d.%01d,%d.%01d",
-               p.age, p.current_id, p.number, p.origin_id, (int)(p.reading[0]),
-               (int)(p.reading[0] * 100) % 100 / 10, (int)(p.reading[1]),
-               (int)(p.reading[1] * 100) % 100 / 10, (int)(p.reading[2]),
-               (int)(p.reading[2] * 100) % 100 / 10, (int)(p.reading[3]),
-               (int)(p.reading[3] * 100) % 100 / 10, (int)(p.reading[4]),
-               (int)(p.reading[4] * 100) % 100 / 10, (int)(p.reading[5]),
-               (int)(p.reading[5] * 100) % 100 / 10);
+    // 1 uint32 (10 chars)
+    // 3 uint8 (3 chars)
+    // 6 floats (formatted as -123.45, so 7 chars)
+    // 9 commas
+    // null-terminator
+    // 1 more char for leeway
+    static char print_packet_buffer[10 + 3 * 3 + 6 * 6 + 9 + 1 + 1];
+    int print_size = snprintf(print_packet_buffer, sizeof(print_packet_buffer), "%lu,%u,%u,%u,%d.%01d,%d.%01d,%d.%01d,%d.%01d,%d.%01d,%d.%01d", 
+        p.age, 
+        p.current_id, 
+        p.number, 
+        p.origin_id, 
+        (int)(p.reading[0]), (int)(p.reading[0] * 100) % 100 / 10, 
+        (int)(p.reading[1]), (int)(p.reading[1] * 100) % 100 / 10, 
+        (int)(p.reading[2]), (int)(p.reading[2] * 100) % 100 / 10, 
+        (int)(p.reading[3]), (int)(p.reading[3] * 100) % 100 / 10, 
+        (int)(p.reading[4]), (int)(p.reading[4] * 100) % 100 / 10,
+        (int)(p.reading[5]), (int)(p.reading[5] * 100) % 100 / 10);
 
-  Serial.println(print_packet_buffer);
+    Serial.println(print_packet_buffer);
 
-  if ((size_t)print_size > sizeof(print_packet_buffer)) {
-    Serial.println("print_packet_buffer too small");
-  }
+    if ((size_t)print_size > sizeof(print_packet_buffer)) {
+        Serial.println("print_packet_buffer too small");
+    }
 }
 
 void loop_tx() {
