567b97ed570a11e59e69a38e3bb3efbd3ee061fd
Dmitry Degtyarev <kvelyognaut@gmail.com>
2019-06-05 12:05:27 -0700

    remove serial printing from id-writer
    add red led as indication of completion

--- tools/id-writer/id-writer.ino
+++ tools/id-writer/id-writer.ino
@@ -3,28 +3,22 @@
 // How to use:
 // Write the ID that you need
 // Upload the code to a board
-// Open the serial monitor
-// Recommended to label the board's id with tape
+// Wait for red LED on the board to turn on
+// Done!
 
 // Write ID here:
-#define ID 133
+#define ID 69
+
+
+#define LED_PIN 13
 
 void setup() {
-    // Wait for serial monitor to open
-    Serial.begin(115200);
-    while (!Serial) {
-        delay(1);
-    }
-
     // Write ID
-    Serial.print("writing ID = ");
-    Serial.println(ID);
     EEPROM.write(EEPROM.length() - 1, ID);
 
-    // Check ID
-    uint8_t current_id = EEPROM.read(EEPROM.length() - 1);
-    Serial.print("current ID = ");
-    Serial.println(current_id);
+    // Turn on red LED
+    pinMode(LED_PIN, OUTPUT);
+    digitalWrite(LED_PIN, HIGH);
 }
 
 void loop() {
